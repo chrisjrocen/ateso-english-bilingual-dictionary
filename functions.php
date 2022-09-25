ec0d644ce7153ac35626504601d32c62e885b633
chrisocen <105217816+chrisocen@users.noreply.github.com>
2022-09-26 00:35:34 +0300

    moved single of single word to the theme

--- functions.php
+++ functions.php
@@ -1,21 +1,8 @@
 <?php
 
 //Enqueue styles and scripts
-  add_action( 'wp_enqueue_scripts', 'chrx_enqueue_scripts' );
-function chrx_enqueue_scripts() {
-	wp_enqueue_style( 'base.css', DICTIONARY_CUSTOM_URL . 'assets/build/css/base.css' , array(), '1.0.0', 'all' );
+add_action('wp_enqueue_scripts', 'chrx_enqueue_scripts');
+function chrx_enqueue_scripts()
+{
+	wp_enqueue_style('base.css', DICTIONARY_CUSTOM_URL . 'assets/build/css/base.css', array(), '1.0.0', 'all');
 }
-
-
-
-
-function get_custom_post_type_template( $archive_template ) {
-	global $wp_query;
-
-	if ( is_post_type_archive ( 'ateso-words' ) ) {
-		$archive_template = dirname( __FILE__ ) . '/templates/ateso-archive.php';
-	}
-	return $archive_template;
-}
-
-add_filter( 'archive_template', 'get_custom_post_type_template' ) ;
